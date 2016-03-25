0d9c5d7d9ac8f22cd0aab5056d2289b67f420f2f
ShvepsyWork <eXr2@mail.ru>
2016-03-25 16:51:35 +0300

    Temp

---
+++ alayzer.c
@@ -0,0 +1,168 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/if.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <linux/ioctl.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <iface.h>
+
+/*
+  Write to nbook ntoa, l234 struct, recvfrom
+*/
+
+int getifconf( uint8_t *intf, struct ifparam *ifp, int mode )
+{
+  int fd;
+  struct sockaddr_in s;
+  struct ifreq ifr;     /* interface name from if.h */
+
+  memset((void *)&ifr, 0, sizeof(struct ifreq));
+  if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) return (-1);
+  sprintf(ifr.ifr_name, "%s", intf);
+
+  if (!mode) goto setmode;
+
+    /* Getting IP address of interface */
+  if (ioctl(fd, SIOCGIFADDR, &ifr) < 0) {
+    perror("ioctl SIOCGIFADDR");
+    return (-1);
+  }
+  memset((void *)&s, 0, sizeof(struct sockaddr_in));
+  memcpy((void *)&s, (void *)&ifr.ifr_addr, sizeof(struct sockaddr));
+  memcpy((void *)&ifp->ip, (void *)s.sin_addr.s_addr, sizeof(__u32));
+
+    /* Getting netmask */
+  if (ioctl(fd, SIOCGIFNETMASK, &ifr) < 0) {
+    perror("ioctl SIOCGIFNETMASK");
+    return (-1);
+  }
+  memset((void *)&s, 0, sizeof(struct sockaddr_in));
+  memcpy((void *)&s, (void *)&ifr.ifr_netmask, sizeof(stuct sockaddr));
+  memcpy((void *)&ifp->mask, (void *)s.sin_addr.s_addr, sizeof(u_long));
+
+    /* Getting MTU */
+  if(ioctl(fd, SIOCGIFMTU, &ifr) < 0) {
+ 	  perror("ioctl SIOCGIFMTU");
+ 	  return (-1);
+  }
+  ifp->mtu = ifr.ifr_mtu;
+
+  /* Getting iface index */
+  if(ioctl(fd, SIOCGIFINDEX, &ifr) < 0) {
+    perror("ioctl SIOCGIFINDEX");
+    return (-1);
+  }
+  ifp->index = ifr.ifr_ifindex
+
+    /* Getting flags */
+  setmode:
+  if(ioctl(fd, SIOCGIFFLAGS, &ifr) < 0) {
+    perror("ioctl SIOCGIFFLAGS");
+    close(fd);
+    return (-1);
+  }
+
+  if (mode) ifr.ifr_flags |= IFF_PROMISC;
+  else ifr.ifr_flags &= ~(IFF_PROMISC);
+
+  /* Flag set */
+  if(ioctl(fd, SIOCSIFFLAGS, &ifr) < 0) {
+    perror(SIOCSIFFLAGS);
+    close(fd);
+    return(-1);
+  }
+
+  return 0;
+}
+
+int getsock_recv(int index)
+{
+  int sd;
+  struct sockaddr_ll s_ll; /* low-level structure for socket */
+
+  if(sd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL)) < 0 ) return (-1); /* create l2 socket */
+
+  memset((void *)&s_ll, 0, sizeof(struct sockaddr_ll));
+
+  s_ll.sll_family = AF_PACKET ;
+  s_ll.sll_ifindex = ifp.index ;          /* or just index? */
+  s_ll.sll_protocol = htons(ETH_P_ALL);
+
+  if(bind(sd, (struct sockaddr_ll *)&s_ll, sizeof(struct sockaddr_ll)) < 0 ) {
+    close(sd);
+    return (-1);
+
+  }
+  return sd;
+}
+
+
+uint8_t buf[ETH_FRAME_LEN];
+
+void mode_off()
+{
+  if(getifconf("eth0", &ifr, PROMISC_MODE_OFF) < 0) {
+    perror("getifconf");
+    exit(-1);
+  }
+  return;
+}
+
+int main() {
+  uint32_t num;
+  int sock_if, rec = 0, ihl = 0;
+  struct ethhdr eth;
+  struct tcphdr tcp;                    /* l2 l3 l4 headers */
+  struct iphdr ip;
+  static struct sigaction act;          /* from <signal.h> to override SIGINT (remove promisc flag) */
+
+  if(getifconf("eth0", &ifp, PROMISC_MODE_ON) < 0 ) {
+    perror("getifconf");
+    return (-1);
+  }
+
+  printf("\tCurrent configuration\nIP:\t%s\n", ntoa(ifp.addr));
+  printf("MASK:\t%s\n", ntoa(ifp.mask));
+  printf("MTU:\t%s\n", ifp.mtu);
+  printf("IfIndex:\t", ifp.index);
+
+  if(sock_if = getsock_recv(ifp.index) < 0) {
+    perror("getsock_recv");
+    return (-1);
+  }
+
+  act.sa_handler = mode_off;             /* Overriding func */
+  sigfillset(&(act.sa_mask));           /* Get current signal mask */
+  sigaction(SIGINT, &act, NULL);       /* Override SIGINT */
+
+  for(;;) {
+    memset(buf, 0, ETH_FRAME_LEN);
+
+    rec = recvfrom(sock_if, (char *)buf, ifp.mtu + 18, 0, NULL, NULL );
+    if(rec < 0 || rec > ETH_FRAME_LEN) {
+      perror("recvfrom");
+      return (-1);
+    }
+  memcpy(&eth, buf, ETH_LEN);
+  memcpy(&ip, buf + ETH_LEN, sizeof(struct iphdr));
+  memcpy(&tcp, buf + ETH + sizeof(struct iphdr), sizeof(struct tcphdr));
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+}
